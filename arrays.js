2629761e20aff311484d05c6c20d3bb810d03f28
Yves Njumbe <ynjumbe23@gmail.com>
2019-04-09 16:07:15 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,5 +31,7 @@ function accessElementInArray (array, index) {
   return array [index]
 }
 
-function destructivelyRemoveElementToBeginningOfArray (array, element) { return array.shift()
+function destructivelyRemoveElementToBeginningOfArray (array) { 
+  array.shift()
+  return array
 }
