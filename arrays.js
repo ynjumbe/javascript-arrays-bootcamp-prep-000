a10a871c5e6af9d5106635ddae096af97c0417cb
Yves Njumbe <ynjumbe23@gmail.com>
2019-04-09 15:54:12 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -17,3 +17,12 @@ function destructivelyAddElementToBeginningOfArray (array, element) {
   array.unshift(element)
   return array
 }
+
+function addElementToEndOfArray (array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray (array, element) {
+  array.push(element)
+  return array
+}
