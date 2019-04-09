440c3c2a47d05ae05c7dd5783bd06a77448b9339
Yves Njumbe <ynjumbe23@gmail.com>
2019-04-09 16:04:27 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -30,3 +30,7 @@ function destructivelyAddElementToEndOfArray (array, element) {
 function accessElementInArray (array, index) {
   return array [index]
 }
+
+function destructivelyRemoveElementToBeginningOfArray (array, elemen) { array.shift()
+return array
+}
