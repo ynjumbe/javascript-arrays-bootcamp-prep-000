ed9a33eb989a7c3abf492d04b79e49b3f79fda4f
Yves Njumbe <ynjumbe23@gmail.com>
2019-04-09 16:08:16 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,7 +31,7 @@ function accessElementInArray (array, index) {
   return array [index]
 }
 
-function destructivelyRemoveElementToBeginningOfArray (array) { 
+function destructivelyRemoveElementFromBeginningOfArray (array) { 
   array.shift()
   
   return array
